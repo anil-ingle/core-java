3073c436a466cf921565e802d0cc646acb1bebc1
anil ingle <anil.krushidj@gmail.com>
2019-01-28 09:01:08 +0530

    Compound operator Logical AND Logical OR opeartor , String COncatination opternary op ..2019128

---
+++ src/org/core/test/StringConcatOperator.java
@@ -0,0 +1,16 @@
+package org.core.test;
+
+public class StringConcatOperator {
+    public static void main(String[] args) {
+        String a="jay";
+        int b=1,c=2,d=3;
+        System.out.println(a+b+c+d);
+        System.out.println(b+c+d+a);
+
+    }
+}
+
+
+//  *
+// *  *
+//*  *   *
