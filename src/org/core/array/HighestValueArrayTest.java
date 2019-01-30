96c53cc906781a67f0e130a92b295dfbab5785c5
anil ingle <anil.krushidj@gmail.com>
2019-01-30 08:52:01 +0530

    Class name change

--- src/org/core/array/HighestValueArrayTest.java
+++ src/org/core/array/HighestValueArrayTest.java
@@ -1,6 +1,6 @@
 package org.core.array;
 
-public class HigestValueArrayTest {
+public class HighestValueArrayTest {
     public static void main(String[] args) {
         int[] arr={20,55,78,44,5,96,4};// int[] arr=new int[7];
         int temp=arr[0];
